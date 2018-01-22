d76cd3c3c086b4c803cf5bbbfe20e962a809e958
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 00:37:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   #your code here
 
   contacts["Freddy Mercury"]
-  [:favorite_icecream_flavors].delete_if("strawberry")
+  [:favorite_icecream_flavors].delete("strawberry")
 
   #remember to return your newly altered contacts hash!
   contacts
