679d758f491609c0cf3a2326c555f1364f2e34bf
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 00:39:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,7 @@ def first_challenge
 
   #your code here
 
-  contacts["Freddy Mercury"]
-  [:favorite_icecream_flavors].delete("strawberry")
+  contacts["Freddy Mercury"][:favorite_icecream_flavors] = ["cookie dough", "mint chip"]
 
   #remember to return your newly altered contacts hash!
   return contacts
